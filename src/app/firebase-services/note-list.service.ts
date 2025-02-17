f949945f5b175ae3f1380d4ca9d13bdedae1ee28
seidenberger <158757732+seidenberger@users.noreply.github.com>
2025-02-17 10:35:14 +0100

    onSnapshot

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject } from '@angular/core';
-import { Firestore, collection, collectionData, doc } from '@angular/fire/firestore';
+import { Firestore, collection, collectionData, doc, onSnapshot} from '@angular/fire/firestore';
 import { Note } from '../interfaces/note.interface';
 
 @Injectable({
@@ -11,11 +11,53 @@ export class NoteListService {
   normalNotes: Note[] = [];
 
   items$;
+  items;
+
+  unsublist;
+  unsubSingel;
+
   firestore = inject(Firestore);
 
   constructor() { 
+
+    this.unsublist = onSnapshot(this.getNotesRef(), (list) =>{
+      list.forEach(element => {
+        // console.log(element.id)
+        console.log(this.steNoteObject(element.data(), element.id))
+      });
+    });
+
+    this.unsubSingel = onSnapshot(this.getSingelDocRef("notes", "43156146164"), (element ) =>{
+    });
+
+  this.unsubSingel();
+
+
     this.items$ = collectionData(this.getNotesRef());
+    this.items = this.items$.subscribe((list) => {
+      list.forEach(element => {
+        console.log(element)
+      });
+    }) 
   }
+
+  ngonDestroy(){
+    this.unsublist();
+    this.items.unsubscribe();
+  }
+
+
+  steNoteObject(obj: any, id: string): Note{
+    return {
+      id: id || "",
+      type: obj.type || "note",
+      title: obj.title || "",
+      content: obj.content || "",
+      marked: obj.marked || false,
+    }
+  }
+
+
     getNotesRef(){
       return collection(this.firestore, 'notes');
     }
@@ -24,8 +66,8 @@ export class NoteListService {
       return collection(this.firestore, 'trash');
     }
 
-    // getSingelDocRef(colID:String, docId:String){
-    //   return doc(collection(this.firestore, colID), docID)
-    // }
+    getSingelDocRef(colID:string, docId:string){
+      return doc(collection(this.firestore, colID), docId)
+    }
   
 }
