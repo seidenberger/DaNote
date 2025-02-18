2fc29f15b856268ebf73b25b71a0c27962e036f8
seidenberger <158757732+seidenberger@users.noreply.github.com>
2025-02-18 10:55:57 +0100

    clear note list

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -10,8 +10,6 @@ export class NoteListService {
   trashNotes: Note[] = [];
   normalNotes: Note[] = [];
 
-  // items$;
-  // items;
 
   unsubNotes;
   unsubTrash;
@@ -26,42 +24,22 @@ export class NoteListService {
   ngonDestroy(){
     this.unsubNotes();
     this.unsubTrash();
-    // this.items.unsubscribe();
   }
 
   subTrashList(){
     return onSnapshot(this.getTrashRef(), (list) =>{
       this.trashNotes = [];
       list.forEach(element => {
-        // console.log(element.id)
-      //   console.log(this.steNoteObject(element.data(), element.id))
-      
       this.trashNotes.push(this.steNoteObject(element.data(), element.id))
     });
     });
   }
-
-  //   this.unsubNotes = onSnapshot(this.getSingelDocRef("notes", "43156146164"), (element ) =>{
-  //   });
-
-  // this.unsubNotes();
-
-
-    // this.items$ = collectionData(this.getNotesRef());
-    // this.items = this.items$.subscribe((list) => {
-    //   list.forEach(element => {
-    //     console.log(element)
-    //   });
-    // }) 
-  // }
 
 
   subNotesList(){
     return onSnapshot(this.getNotesRef(), (list) =>{
       this.normalNotes = [];
       list.forEach(element => {
-        // console.log(element.id)
-      //   console.log(this.steNoteObject(element.data(), element.id))
       this.normalNotes.push(this.steNoteObject(element.data(), element.id))
       });
       
@@ -86,10 +64,6 @@ export class NoteListService {
     getTrashRef(){
       return collection(this.firestore, 'trash');
     }
-
-    // getTrashRef() {
-
-    // }
 
     getSingelDocRef(colID:string, docId:string){
       return doc(collection(this.firestore, colID), docId)
--- src/app/note-list/note-list.component.ts
+++ src/app/note-list/note-list.component.ts
@@ -20,8 +20,6 @@ export class NoteListComponent {
   status: "notes" | "trash" = "notes";
 
   constructor(private noteService: NoteListService) {
-    // constructor(public noteService: NoteListService) {
-    // this.noteList = this.getDummyData()
   }
 
   getList() {
@@ -40,47 +38,5 @@ export class NoteListComponent {
       this.favFilter = "all";
     }
   }
-
-
-
-
-  // getDummyData(): Note[] {
-  //   return [
-  //     {
-  //       id: "21sasd561dd4sdf",
-  //       type: "note",
-  //       title: "Block, Inline, and Inline-Block",
-  //       content: "https://www.youtube.com/watch?v=x_i2gga-sYg",
-  //       marked: true,
-  //     },
-  //     {
-  //       id: "25sd4f561w54sdf",
-  //       type: "note",
-  //       title: "css selector",
-  //       content: `kind p > b   (direktes kind) 
-  //       nachfahren p b  (alle nachfahren)
-  //       geschwister p ~ b (auf gleicher ebene ist VOR dem p ein b)`,
-  //       marked: true,
-  //     },
-  //     {
-  //       id: "54a4s6d546ff",
-  //       type: "note",
-  //       title: "aufräumen",
-  //       content: "Wohnzimmer saugen",
-  //       marked: false,
-  //     },
-  //     {
-  //       id: "2a35s4d654a6s4d",
-  //       type: "note",
-  //       title: "links",
-  //       content: `Reihenfolge: a:visited 
-  //       a:focus 
-  //       a:hover 
-  //       a:active
-  //       merkspruch: LoVe HAte`,
-  //       marked: true,
-  //     }
-  //   ];
-  // }
 
 }
